e64478a7074f87c15203ee111e9aa4efc3ebe7c1
SaraCastro124 <sara1330castro@gmail.com>
2023-03-15 15:13:52 -0300

    l

---
+++ RepasoPersona.Core/Cliente.cs
@@ -0,0 +1,5 @@
+namespace RepasoPersona.Core;
+
+public class Cliente
+{
+}
---
+++ RepasoPersona.Core/Cuenta.cs
@@ -0,0 +1,10 @@
+
+namespace RepasoPersona.Core
+{
+    public class Cuenta
+    {
+        public double Saldo {get; private set;}
+        public int Cbu {get; private set;}
+        public double Efectivo {get; private set;} 
+    }
+}
